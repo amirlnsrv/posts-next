989e90fe218f2ced89fab9c99418f6d1f266a8bc
amirlnsrv <amir.lansarov@gmail.com>
2025-05-05 15:33:04 +0600

    feat: added posts in blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+
+import { BASE_URL } from "@/baseUrl";
 import React from "react";
 
+import styles from "./Home.module.css";
+
 export default function Blog() {
-  return <div>Blog</div>;
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const res = await fetch(BASE_URL);
+        const data = await res.json();
+
+        setPosts(data);
+      };
+
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <div className={styles.blog}>
+      {posts.map((item) => (
+        <div key={item.id} className={styles.post}>
+          <h2>{item.title}</h2>
+          <p>{item.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
