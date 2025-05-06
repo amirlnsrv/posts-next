1a65cc96fe282d7df43d13d3867a217bce7e036b
amina5789 <merzamedovaa@gmail.com>
2025-05-06 09:44:30 +0300

    fix: deleteBtn

--- src/components/BlogItem/BlogItem.tsx
+++ src/components/BlogItem/BlogItem.tsx
@@ -6,11 +6,11 @@ import { Post } from "@/types/postType";
 import styles from "./BlogItem.module.css";
 import { Button } from "@/ui/Button";
 import { useRouter } from "next/navigation";
+import { DeleteButton } from "@/components/DeleteButton";
 
 type State = {
   isDeleted: boolean;
 };
-
 
 type Action = { type: "DELETE" };
 
@@ -27,14 +27,14 @@ export const BlogItem: React.FC<Post> = ({ title, body, id }) => {
   const router = useRouter();
 
   const [state, dispatch] = useReducer(reducer, { isDeleted: false });
-  const handleDelete = () => {
-    dispatch({ type: "DELETE" });
-  };
   if (state.isDeleted) return null;
 
   return (
     <div className={styles.post}>
-      <div className={`${styles.postBody} cursor-pointer`} onClick={() => router.push(`/${id}`)}>
+      <div
+        className={`${styles.postBody} cursor-pointer`}
+        onClick={() => router.push(`/${id}`)}
+      >
         <h2>{title}</h2>
         <p>{body}</p>
       </div>
@@ -42,12 +42,8 @@ export const BlogItem: React.FC<Post> = ({ title, body, id }) => {
         <Button className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
           Изменить
         </Button>
-        <Button
-          onClick={handleDelete}
-          className="cursor-pointer bg-transparent hover:bg-red-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-        >
-          Удалить
-        </Button>
+
+        <DeleteButton postId={String(id)} onDelete={() => dispatch({ type: "DELETE" })} /> 
       </div>
     </div>
   );
